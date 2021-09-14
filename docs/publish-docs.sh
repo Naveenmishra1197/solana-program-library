7af80aef85caf129c59e0cc006b8920bea709bc6
RITESH KUMAR SINGH <66211812+singhritesh750@users.noreply.github.com>
2021-09-14 20:40:56 +0530

    added the braces around vercel token in line 24

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -21,7 +21,7 @@ cat > "$CONFIG_FILE" <<EOF
 }
 EOF
 
-[[ -n $VERCEL_TOKEN ]] || {
+[[ -n ${{VERCEL_TOKEN}} ]] || {
   echo "VERCEL_TOKEN is undefined.  Needed for Vercel authentication."
    exit 1
 }
