90f728c0cb0bed3923560a2a7935181711f68833
RITESH KUMAR SINGH <66211812+singhritesh750@users.noreply.github.com>
2021-09-14 18:57:31 +0530

    Update publish-docs.sh

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -23,6 +23,6 @@ EOF
 
 [[ -n $VERCEL_TOKEN ]] || {
   echo "VERCEL_TOKEN is undefined.  Needed for Vercel authentication."
-  exit 1
+#   exit 1
 }
 vercel deploy . --local-config="$CONFIG_FILE" --confirm --token "$VERCEL_TOKEN" "$PRODUCTION"
