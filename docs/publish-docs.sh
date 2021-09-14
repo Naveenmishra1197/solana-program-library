6198da62e5e504a5d3e67ee3cdd98e5a52375adc
RITESH KUMAR SINGH <66211812+singhritesh750@users.noreply.github.com>
2021-09-14 20:46:37 +0530

    explicity adding vercel token into the file for static taken passing

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -21,7 +21,9 @@ cat > "$CONFIG_FILE" <<EOF
 }
 EOF
 
-[[ -n ${{VERCEL_TOKEN}} ]] || {
+VERCEL_TOKEN=3NFKE4wq2daxKnOrqgR7mGTH
+
+[[ -n $VERCEL_TOKEN ]] || {
   echo "VERCEL_TOKEN is undefined.  Needed for Vercel authentication."
    exit 1
 }
