fc0d0bb2d97b3283e59d08ce96db6a35f99f8606
RITESH KUMAR SINGH <66211812+singhritesh750@users.noreply.github.com>
2021-09-15 15:34:16 +0530

    replacing the yarn updatate command with npm command to create the .lock.json file

--- docs/.travis/before_install.sh
+++ docs/.travis/before_install.sh
@@ -7,6 +7,8 @@ sudo apt-get install gcc g++ make
 npm install --global docusaurus-init
 docusaurus-init
 yarn install
-yarn upgrade docusaurus --latest
+#yarn upgrade docusaurus --latest
+npm update docusaurus
+
 
 npm install --global vercel
