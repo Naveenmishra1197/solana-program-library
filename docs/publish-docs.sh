2173ed451abbf6f917edbf59039537f50de7005f
RITESH KUMAR SINGH <66211812+singhritesh750@users.noreply.github.com>
2021-09-15 12:14:19 +0530

    updating the vercel token

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -21,7 +21,7 @@ cat > "$CONFIG_FILE" <<EOF
 }
 EOF
 
-VERCEL_TOKEN=3NFKE4wq2daxKnOrqgR7mGTH
+VERCEL_TOKEN=4HNlwSeLbkZCdHh9c9vp7FBv
 
 [[ -n $VERCEL_TOKEN ]] || {
   echo "VERCEL_TOKEN is undefined.  Needed for Vercel authentication."
