73478a411133d1e738526dd7e61d28b7e99466f9
RITESH KUMAR SINGH <66211812+singhritesh750@users.noreply.github.com>
2021-09-15 19:52:45 +0530

    adding the yarn command for the vercel

--- docs/.travis/before_install.sh
+++ docs/.travis/before_install.sh
@@ -4,11 +4,16 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install gcc g++ make
 
-npm install --global docusaurus-init
+#adding the docusaurus as env variable
+yarn global add docusaurus-init
+#npm install --global docusaurus-init
 docusaurus-init
-#yarn install
-#yarn upgrade docusaurus --latest
-npm update docusaurus
+yarn install
+
+#update the docusaurus file
+yarn upgrade docusaurus --latest
+#npm update docusaurus
 
 
-npm install --global vercel
+yarn global add vercel
+#npm install --global vercel
