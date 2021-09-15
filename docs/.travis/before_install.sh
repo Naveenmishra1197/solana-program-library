543ac8d6b0c16250352887f8afeb18885b2f9f57
RITESH KUMAR SINGH <66211812+singhritesh750@users.noreply.github.com>
2021-09-15 15:10:39 +0530

    added the "gcc" and upgraded the "docusaurus"

--- docs/.travis/before_install.sh
+++ docs/.travis/before_install.sh
@@ -1,9 +1,11 @@
 # |source| this file
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install -y nodejs
+sudo apt-get install -y nodejs
+sudo apt-get install gcc g++ make
 
 npm install --global docusaurus-init
 docusaurus-init
+yarn upgrade docusaurus --latest
 
 npm install --global vercel
