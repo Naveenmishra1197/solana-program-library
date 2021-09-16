0b2fb8364e08e752e9d459acc1f4d2d30392c0cc
RITESH KUMAR SINGH <66211812+singhritesh750@users.noreply.github.com>
2021-09-17 03:26:59 +0530

    removing all the unnecesssary "ls" commands from this file

--- docs/build.sh
+++ docs/build.sh
@@ -7,13 +7,11 @@ cd "$(dirname "$0")"
 source ../ci/env.sh
 
 yarn global add docusaurus-init 
-ls
+
 #npm install --global docusaurus-init
 cd docusaurus/
-ls
 docusaurus-init
 
-ls
 #mv docs-examples-from-docusaurus docs
 
 #cd ./website
@@ -22,30 +20,26 @@ ls
 #Launching the server behind a proxy
 #SET NO_PROXY=localhost
 #npm run start
-ls ../
-ls ../../
+
 # checking the current versiona and updating the docusaurus from the website directory
 cd ./website
-ls
+
 
 cd ../../
 yarn outdated docusaurus
 #npm outdated docusaurus
 yarn upgrade docusaurus --latest
 #npm update docusaurus
-ls
+
 
 yarn install
 # Build from /src into /build
 yarn build
 #npm run build
-ls
 
 # Publish only from merge commits and release tags
 if [[ -n $CI ]]; then
-  echo "cheking the CI is not null or not"
   if [[ -z $CI_PULL_REQUEST ]]; then
-    echo "checking the CI_PULL_REQUEST value is zero or not"
     ./publish-docs.sh
   fi
 fi
