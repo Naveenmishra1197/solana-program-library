e53cff2bb33efadcb654c5dd29c1455eaa9b0f8b
RITESH KUMAR SINGH <66211812+singhritesh750@users.noreply.github.com>
2021-09-17 02:16:29 +0530

    installing the "Yarn package manager" and removing the docusaurus to see the requirement of docusaurus

--- docs/.travis/before_install.sh
+++ docs/.travis/before_install.sh
@@ -2,16 +2,22 @@
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# To install the Yarn package manager, run:
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
 sudo apt-get install gcc g++ make
 
-#adding the docusaurus as env variable
-yarn global add docusaurus-init
-#npm install --global docusaurus-init
-docusaurus-init
+# #adding the docusaurus as env variable
+# yarn global add docusaurus-init
+# #npm install --global docusaurus-init
+# docusaurus-init
 yarn install
 
 #update the docusaurus file
-yarn upgrade docusaurus --latest
+# yarn upgrade docusaurus --latest
 #npm update docusaurus
 
 
