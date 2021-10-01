1cad71be81ab9f2290f77d74ecf3237be47da95d
Naveenmishra1197 <76965435+Naveenmishra1197@users.noreply.github.com>
2021-10-01 15:13:01 +0530

    update vercel token in publish.sh

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -30,7 +30,7 @@ EOF
 # touch .env.production
 # echo VERCEL_TOKEN = "$VERCEL_TOKEN" > .env.production
 
-# # VERCEL_TOKEN="4V4RI2LA8Y1ZrUaS2Q9tDMD3"
+# # VERCEL_TOKEN="2bwWlTgeTGvo63xZViVa2XhI"
 
 [[ -n $VERCEL_TOKEN ]] || {
   echo "VERCEL_TOKEN is undefined.  Needed for Vercel authentication."
