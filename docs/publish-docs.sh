e0e5a9d1f5a88d0bd64c90ac07eaf9760f300bb3
Naveenmishra1197 <76965435+Naveenmishra1197@users.noreply.github.com>
2021-10-01 13:19:44 +0530

    Update publish-docs.sh

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -17,9 +17,14 @@ fi
 cat > "$CONFIG_FILE" <<EOF
 {
   "name": "$PROJECT_NAME",
-  "scope": "solana-labs"
+  "scope": "Naveenmishra1197"
 }
 EOF
+
+# touch .env.production
+# echo VERCEL_TOKEN = "$VERCEL_TOKEN" > .env.production
+
+# # VERCEL_TOKEN="4V4RI2LA8Y1ZrUaS2Q9tDMD3"
 
 [[ -n $VERCEL_TOKEN ]] || {
   echo "VERCEL_TOKEN is undefined.  Needed for Vercel authentication."
